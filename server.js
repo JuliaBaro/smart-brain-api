b6285ce0b59d438f205d394cec4bc639779fb82c
unknown <baro.julia@gmail.com>
2020-05-08 19:44:45 +0200

    udpating database

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-lively-82494',
     user : 'Julia',
     password : '',
     database : "'smart-brain'"
